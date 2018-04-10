2c6802ce8ce63bb1cf1c07f20774e281c14cb342
Taegoo Kang <Taegoo@Taegoos-MacBook-Pro-16.local>
2018-04-10 17:31:26 +0900

    Initializes deploy script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,5 @@
-var SafeMath = artifacts.require("SafeMath");
-var AdminOwnable = artifacts.require("AdminOwnable");
-var ERC20Standard = artifacts.require("ERC20Standard");
-var PoSTokenCustomStandard = artifacts.require("PoSTokenCustomStandard");
 var MaestroToken = artifacts.require("MaestroToken");
 
 module.exports = function(deployer) {
-  deployer.deploy(
-		SafeMath,
-		AdminOwnable,
-  	ERC20Standard,
-  	PoSTokenCustomStandard
-	);
-	deployer.link(SafeMath, MaestroToken);
   deployer.deploy(MaestroToken);
 };
