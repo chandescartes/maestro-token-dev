91818d93fc2f87c244fafa8aa0a2158d9c5a44b0
Chanyoung Ryu <chandescartes@gmail.com>
2018-05-14 15:54:21 +0900

    Add build/ folder. Version #1 for test crowdsale on Ropsten.

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,10 +1,10 @@
 const MaestroToken = artifacts.require("MaestroToken");
 
 module.exports = function(deployer, network, accounts) {
-    const initialSupplyWithoutDecimals = 9999;
-    const lockupDurationS1 = 180;
-    const lockupDurationS2 = 180;
-    const lockupDurationTeam = 365;
+    const initialSupplyWithoutDecimals = 1000000000;
+    const lockupDurationS1 = 1;
+    const lockupDurationS2 = 1;
+    const lockupDurationTeam = 2;
 
     deployer.deploy(MaestroToken, initialSupplyWithoutDecimals, lockupDurationS1, lockupDurationS2, lockupDurationTeam);
 };
--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -1,17 +1,18 @@
 const MaestroCrowdsale = artifacts.require("MaestroCrowdsale");
 
 module.exports = function(deployer, network, accounts) {
-    const rate = 1000;
-    const wallet = "0x62e850DC2bCa2A66a6392c3c1310e79bd68cCB01";
-    const cap = 10 * (10 ** 18); // Ten ether
+    const rate = 10000;
+    const wallet = "0x4f8b41416f28E686A6eAF5B25b90F9197DF47b77";
+    const cap = 100000000000000000000;
+    const token = "0xb0bd6577acc126c6f5e5cb853283e9dae7b41104";
 
     // const latest = res.timestamp;
     // const openingTime = latest + 60; // One minute
     // const closingTime = openingTime + 600; // Ten minutes
 
-    const openingTime = 1526149864;
-    const closingTime = openingTime + 600; // Ten minutes
+    const openingTime = 1526280000;
+    const closingTime = 1526452800; // Ten minutes
     
     // Ropsten: 0x210f043c348ea032b12114001f194c90490f978f
-    deployer.deploy(MaestroCrowdsale, openingTime, closingTime, rate, wallet, cap, "0x210f043c348ea032b12114001f194c90490f978f");
+    deployer.deploy(MaestroCrowdsale, openingTime, closingTime, rate, wallet, cap, token);
 };
