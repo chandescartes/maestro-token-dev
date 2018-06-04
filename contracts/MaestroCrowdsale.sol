ccb5c59b2d6e5a9b42644b7b625c13d8d3fb7891
Chanyoung Ryu <chandescartes@gmail.com>
2018-06-04 16:21:23 +0900

    Add whitelist logic

--- contracts/MaestroCrowdsale.sol
+++ contracts/MaestroCrowdsale.sol
@@ -127,6 +127,33 @@ contract MaestroCrowdsale {
         return weiRaised >= cap;
     }
 
+    /***************************/
+    /*                         */
+    /*   WhitelistedCrowdsale  */
+    /*                         */
+    /***************************/
+
+    mapping(address => bool) public whitelist;
+
+    modifier isWhitelisted(address _beneficiary) {
+        require(whitelist[_beneficiary]);
+        _;
+    }
+
+    function addToWhitelist(address _beneficiary) external onlyOwner {
+        whitelist[_beneficiary] = true;
+    }
+
+    function addManyToWhitelist(address[] _beneficiaries) external onlyOwner {
+        for (uint256 i = 0; i < _beneficiaries.length; i++) {
+            whitelist[_beneficiaries[i]] = true;
+        }
+    }
+
+    function removeFromWhitelist(address _beneficiary) external onlyOwner {
+        whitelist[_beneficiary] = false;
+    }
+
     /**
      * Constructor
      * Variables from inherited contracts
@@ -172,7 +199,7 @@ contract MaestroCrowdsale {
     /**
      * Override parent contracts to combine implementation of CappedCrowdsale and TimedCrowdsale
      */
-    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileOpen {
+    function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileOpen isWhitelisted(_beneficiary) {
         require(_beneficiary != address(0));
         require(_weiAmount != 0);
         require(weiRaised.add(_weiAmount) <= cap);
@@ -200,6 +227,7 @@ contract MaestroCrowdsale {
     function returnTrue() public pure returns (bool) {
         return true;
     }
+
 
     /*************************/
     /*                       */
--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -4,14 +4,15 @@ module.exports = function(deployer, network, accounts) {
     const rate = 10000;
     const wallet = "0x0CD5d8747da0790c9a29E4F34bcf46cc3Bdf8976";
     const cap = 100000000000000000000;
-    const token = "0x45c1f4b458af1ee1f736d5423a147c9bb89702b8";
-
+    // const token = "0x45c1f4b458af1ee1f736d5423a147c9bb89702b8";
+    const token = "0xa8c6612a86773e650fadf35a28aae0e2ea225d53"; // Ganache
+    
     // const latest = res.timestamp;
     // const openingTime = latest + 60; // One minute
     // const closingTime = openingTime + 600; // Ten minutes
 
-    const openingTime = 1526367600;
-    const closingTime = 1526454000; // Ten minutes
+    const openingTime = 1528097455;
+    const closingTime = openingTime + 36000; // Ten minutes
     
     // Ropsten: 0x210f043c348ea032b12114001f194c90490f978f
     deployer.deploy(MaestroCrowdsale, openingTime, closingTime, rate, wallet, cap, token);
